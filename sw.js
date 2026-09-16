3f8d77abe9d7a0e062b369e110a390f9bc7186c1
GS-MontageApp <m3-jost@gmx.de>
2026-09-16 16:48:02 +0200

    Update service worker version to v53

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// === KÖTTELMANN APP - Service Worker v52 ===
-const CACHE_NAME = 'koettelmann-v52';
+// === KÖTTELMANN APP - Service Worker v53 ===
+const CACHE_NAME = 'koettelmann-v53';
 const ASSETS_TO_CACHE = [
   './index.html',
   './manifest.json',
