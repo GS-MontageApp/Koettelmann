8fddee8af0c15c1b2953a98bf299c8e1039280c9
GS-MontageApp <m3-jost@gmx.de>
2026-09-16 14:35:53 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// === KÖTTELMANN APP - Service Worker v34 ===
-const CACHE_NAME = 'koettelmann-v34';
+// === KÖTTELMANN APP - Service Worker v35 ===
+const CACHE_NAME = 'koettelmann-v35';
 const ASSETS_TO_CACHE = [
   './index.html',
   './manifest.json',
